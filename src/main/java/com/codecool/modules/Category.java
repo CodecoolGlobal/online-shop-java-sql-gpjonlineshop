b7dd227a10fbfff3348b9b9eacb48aa48c7aa2e5
Grelory <gmpwm99@gmail.com>
2020-05-05 10:03:47 +0200

    add Category class

---
+++ src/main/java/com/codecool/modules/Category.java
@@ -0,0 +1,17 @@
+package com.codecool.modules;
+
+import java.util.ArrayList;
+
+public class Category {
+
+    private int id;
+    private String name;
+    private boolean isAvailable;
+    private ArrayList<Product> products;
+
+    public Category(String name) {
+
+    }
+
+
+}
