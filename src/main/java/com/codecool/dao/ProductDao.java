80cc7a777922aeb15c1271c00809b3f3aa979826
pietia <piotrstepak@gmail.com>
2020-05-05 18:04:42 +0200

    add checkProductAvailibility method

--- src/main/java/com/codecool/dao/ProductDao.java
+++ src/main/java/com/codecool/dao/ProductDao.java
@@ -8,18 +8,18 @@ import java.util.List;
 
 public class ProductDao extends Dao {
 
-    public List<Product> getProducts() {
+    public List<Product> getProducts(String productCategory) {
         List<Product> products = new ArrayList<>();
         connect();
 
         try {
-            ResultSet results = statement.executeQuery("SELECT * FROM vacuumCleaners;");
+            ResultSet results = statement.executeQuery(String.format("SELECT * FROM %s;", productCategory));
             while (results.next()) {
                 int id = results.getInt("id");
                 String name = results.getString("name");
                 int price = results.getInt("price");
                 int amount = results.getInt("amount");
-                Category category = join.// jak pobrac?
+                Category category = new Category(results.getString(" ")); //TODO pobranie category name
 
                 Product product = new Product(id, name, price, amount, category);
                 products.add(product);
@@ -30,6 +30,30 @@ public class ProductDao extends Dao {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return products;
+        return products; // czy zwracac w postaci stringa
+    }
+
+    public Product checkProductAvailiability(String productCategory, String productName) {//zwraca info o jednym konkretnym produkcie
+        Product product = null;
+        connect();
+
+        try {
+            ResultSet results = statement.executeQuery(String.format("SELECT * FROM %s WHERE name = %s;", productCategory, productName));
+            while (results.next()) {
+                int id = results.getInt("id");
+                String name = results.getString("name");
+                int price = results.getInt("price");
+                int amount = results.getInt("amount");
+                Category category = new Category(results.getString(" ")); //TODO pobranie category name
+
+                product = new Product(id, name, price, amount, category);
+            }
+            results.close();
+            statement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return product;
     }
 }
