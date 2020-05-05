f885b619444707b93b4931ef83c216d93ae7664b
pietia <piotrstepak@gmail.com>
2020-05-05 08:18:25 +0200

    add Admin class

--- src/main/java/com/codecool/user/Customer.java
+++ src/main/java/com/codecool/user/Customer.java
@@ -1,3 +1,5 @@
+package com.codecool.user;
+
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
--- src/main/java/com/codecool/user/User.java
+++ src/main/java/com/codecool/user/User.java
@@ -1,3 +1,5 @@
+package com.codecool.user;
+
 import java.util.Date;
 
 public abstract class User {
